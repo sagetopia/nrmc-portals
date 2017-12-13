644413670bcc097b8bcdb678db620c9b1ec7e46d
Sagetopia <developer@sagetopia.com>
2017-12-13 15:57:46 -0500

    Update for CAP layout

---
+++ assets/scripts/main.js
@@ -0,0 +1,38 @@
+(function($) {
+	$(document).ready(function() {
+		$('.faq-accordion').accordion({
+          header: '.faq-title',
+          heightStyle: 'content'
+        });
+
+        $('.tooltip').on('click', function(e) {
+        	e.preventDefault();
+
+        	var tooltip = $(this).data('definition');
+        	$('.tooltip-content.' + tooltip).addClass('active');
+        });
+
+        $('.tooltip-content').on('click', function() {
+        	$(this).removeClass('active');
+        });
+
+        $('.login a').on('click', function(e) {
+            e.preventDefault();
+            var scrolltop = $('#login').offset().top - $('.site-header').outerHeight();
+            $("html, body").animate({ scrollTop: scrolltop }, 600);
+            return false;
+        });
+
+        $('select').each(function() {
+            var placeholder = $(this).attr('placeholder');
+            $(this).select2({ 
+                placeholder: placeholder,
+                minimumResultsForSearch: Infinity,
+            });
+        });
+
+        $('.btn-trigger').on('click', function() {
+            $('body').toggleClass('scroll-lock');
+        });
+	});
+})(jQuery);
